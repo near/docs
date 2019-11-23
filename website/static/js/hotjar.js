20ea340a40e25f142b4748c625f2dc53cc18e3bd
Sherif Abushadi <sherif@amgando.org>
2019-11-24 00:45:03 +0300

    whitelist Algolia search bar for hotjar visibility (#129)

--- website/static/js/hotjar.js
+++ website/static/js/hotjar.js
@@ -7,3 +7,16 @@
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+/**
+    How to Whitelist Elements and Keystrokes
+    https://help.hotjar.com/hc/en-us/articles/115015563287-How-to-Whitelist-Elements-and-Keystrokes
+
+    chose to capture Algolia search terms in real time through hotjar to see what people did once
+    they searched.  Algolia reports similar data but this ties it in nicely with on-page behavior
+ */
+(function(){
+    window.addEventListener('DOMContentLoaded', (event) => {
+        document.querySelector('input#search_input_react').classList.add('data-hj-whitelist')
+    });
+})()
