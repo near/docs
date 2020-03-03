391ddecbd1a2c30a75b79b4fb7e75f4095adb64d
Sherif Abushadi <sherif@amgando.org>
2020-03-03 11:29:30 +0300

    ignore errors while testing links to make sure we cleanup

--- website/test-links.sh
+++ website/test-links.sh
@@ -11,6 +11,9 @@ npx serve build/near-docs &
 # wait til the server is responding
 npx wait-on http://localhost:5000/
 
+# ignore errors since broken-link-checker exits with nonzero error if any links break
+set +e
+
 # crawl site and check all links
 npx broken-link-checker -gro --exclude "localhost:3030" --exclude "https://github.com/nearprotocol/docs/tree/master/docs" --exclude "https://youtu.be" --exclude "http://near.ai/wbs" --exclude "https://www.youtube.com" --host-requests 2 "http://localhost:5000"
 
