8e72115d1dde7a756e8ea6d2e8a2170e89859124
Yifang Ma <33007476+icerove@users.noreply.github.com>
2021-03-10 06:44:42 -0800

    add UTM (#622)

--- website/static/js/mixpanel.js
+++ website/static/js/mixpanel.js
@@ -49,4 +49,42 @@ window.addEventListener("load", function(){
       mixpanel.track("Search the docs", {'content': value})
     }
   }
+
+  // UTM
+  function getQueryParam(url, param) {
+    // Expects a raw URL
+    param = param.replace(/[[]/, "\[").replace(/[]]/, "\]");
+    let regexS = "[\?&]" + param + "=([^&#]*)"
+    let regex = new RegExp( regexS )
+    let results = regex.exec(url)
+    if (results === null || (results && typeof(results[1]) !== 'string' && results[1].length)) {
+      return '';
+    } 
+    return decodeURIComponent(results[1]).replace(/\W/gi, ' ');
+  }
+
+  function campaignParams() {
+    var campaign_keywords = 'utm_source utm_medium utm_campaign utm_content utm_term'.split(' ')
+        , kw = ''
+        , params = {}
+        , first_params = {};
+    var index;
+    for (index = 0; index < campaign_keywords.length; ++index) {
+      kw = getQueryParam(document.URL, campaign_keywords[index]);
+      if (kw.length) {
+        params[campaign_keywords[index] + ' [last touch]'] = kw;
+      }
+    }
+    for (index = 0; index < campaign_keywords.length; ++index) {
+      kw = getQueryParam(document.URL, campaign_keywords[index]);
+      if (kw.length) {
+        first_params[campaign_keywords[index] + ' [first touch]'] = kw;
+      }
+    }
+    mixpanel.people.set(params);
+    mixpanel.people.set_once(first_params);
+    mixpanel.register(params);
+  }
+
+  campaignParams()
 });
