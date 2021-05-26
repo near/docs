8684a8fc593fbcbd31ff77c63887a2870a78f652
Josh <thisisjoshford@gmail.com>
2021-05-25 17:27:09 -0700

    remove broken-link-checker & use linkinator

--- website/test-links.sh
+++ website/test-links.sh
@@ -12,11 +12,12 @@ npx serve build/near-docs &
 # wait til the server is responding
 npx wait-on http://localhost:5000/
 
-# ignore errors since broken-link-checker exits with nonzero error if any links break
+# ignore errors since link check exits with nonzero error if any links break
 set +e
 
 # crawl site and check all links
-npx broken-link-checker -gro --exclude "localhost:3030" --exclude "https://twitter.com" --exclude "http://127.0.0.1:3019"  --exclude "http://127.0.0.1:4000"  --exclude "localhost:1234" --exclude "https://youtu.be" --exclude "https://www.youtube.com" --host-requests 2 http://localhost:5000 --exclude "https://rpc.testnet.near.org" --exclude "https://rpc.betanet.near.org" --exclude "https://rpc.near.org"  --exclude "https://crates.io/crates/near-sdk" --exclude "https://wasmbyexample.dev" --exclude "https://near.zavodil.ru/?pools=" --exclude "https://staking.dokia.cloud/staking/near/validators" --exclude "https://www.timeanddate.com/moon/phases/" --exclude "https://github.com/near/docs/tree/master/docs"
+npx linkinator http://localhost:5000 --recurse
+#npx broken-link-checker - gro --exclude "localhost:3030" --exclude "https://twitter.com" --exclude "http://127.0.0.1:3019"  --exclude "http://127.0.0.1:4000"  --exclude "localhost:1234" --exclude "https://youtu.be" --exclude "https://www.youtube.com" --host-requests 2 http://localhost:5000 --exclude "https://rpc.testnet.near.org" --exclude "https://rpc.betanet.near.org" --exclude "https://rpc.near.org"  --exclude "https://crates.io/crates/near-sdk" --exclude "https://wasmbyexample.dev" --exclude "https://near.zavodil.ru/?pools=" --exclude "https://staking.dokia.cloud/staking/near/validators" --exclude "https://www.timeanddate.com/moon/phases/" --exclude "https://github.com/near/docs/tree/master/docs"
 
 # record exit status of link check
 status=$?
