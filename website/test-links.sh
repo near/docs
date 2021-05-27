3befb1d59802c2da8eeccb5f1b63f98c21fc153e
Josh <thisisjoshford@gmail.com>
2021-05-27 15:54:26 -0700

    update test-links script

--- website/test-links.sh
+++ website/test-links.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 set -e
 
 # build the project
@@ -8,6 +6,7 @@ npm run build
 
 # serve up files for testing to port 5000 (default)
 npx serve build/near-docs &
+SERVE_PID=$!
 
 # wait til the server is responding
 npx wait-on http://localhost:5000/
@@ -16,14 +15,14 @@ npx wait-on http://localhost:5000/
 set +e
 
 # crawl site and check all links
-npx linkinator http://localhost:5000 --recurse
-#npx broken-link-checker - gro --exclude "localhost:3030" --exclude "https://twitter.com" --exclude "http://127.0.0.1:3019"  --exclude "http://127.0.0.1:4000"  --exclude "localhost:1234" --exclude "https://youtu.be" --exclude "https://www.youtube.com" --host-requests 2 http://localhost:5000 --exclude "https://rpc.testnet.near.org" --exclude "https://rpc.betanet.near.org" --exclude "https://rpc.near.org"  --exclude "https://crates.io/crates/near-sdk" --exclude "https://wasmbyexample.dev" --exclude "https://near.zavodil.ru/?pools=" --exclude "https://staking.dokia.cloud/staking/near/validators" --exclude "https://www.timeanddate.com/moon/phases/" --exclude "https://github.com/near/docs/tree/master/docs"
+npx linkinator http://localhost:5000
 
 # record exit status of link check
 status=$?
 
 # cleanup
-kill $(ps -eaf | awk '/npx serve/ {print $2}' | head -1)
+echo $SERVE_PID
+kill -9 $SERVE_PID
 
 # set exit status to link check's exit status, so script fails correctly on CI
 exit $status
