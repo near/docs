843b33ffd4e941ce5834d756d2bbb6a41e369c1d
Bohdan Khorolets <bogdan@khorolets.com>
2021-08-16 13:27:26 +0300

    Move custom css from separate file to custom.css

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -151,7 +151,6 @@ const siteConfig = {
     "https://fonts.googleapis.com/css2?family=Inter:wght@400;600;900&family=Source+Code+Pro:ital,wght@0,400;0,600;1,400;1,600&display=swap",
     "/css/copy-code-button.css",
     "/css/landing-page.css",
-    "/css/custom-striped-tables-reverse.css",
     "https://near.org/wp-content/themes/near-19/assets/dist/near.min.css",
   ],
 
