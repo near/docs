2e3d01294382f98f34fb9db5efb637bc988c6a18
kushagra <bubnakushagra@gmail.com>
2022-03-11 17:23:26 +0530

    added comment

--- website/src/pages/Demo.js
+++ website/src/pages/Demo.js
@@ -4,8 +4,10 @@ import Editor from '../pages/editor.js'
 
 
 function Demo() {
+    // Adding the Editor component
     return (
         <Fragment>
+
             {/* <Editor /> */}
             <a className="editor" href="/editor">Try it with Editor</a>
         </Fragment>
