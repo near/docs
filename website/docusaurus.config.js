d54f8e6687c1baafb82e39657b3f32f1bd2b9a9a
NEAR Docs Generator <docs-generator@near>
2022-09-07 19:58:35 +0300

    docs-generator

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -176,7 +176,7 @@ module.exports = {
       apiKey: "129d0f429e1bb0510f0261dda1e88ed4",
       indexName: "near",
       // Optional: see doc section below
-      contextualSearch: false,
+      contextualSearch: true,
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
       externalUrlRegex: "near-sdk\\.io",
       // Optional: Algolia search parameters
