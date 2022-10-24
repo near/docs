b226f0985aaa8188598cb9b72036c726aa7b3af7
Max <max.khrp@gmail.com>
2022-10-24 19:04:37 +0300

    wip

--- website/src/theme/SearchBar/UnifiedSearchModal.tsx
+++ website/src/theme/SearchBar/UnifiedSearchModal.tsx
@@ -13,7 +13,7 @@ import {
 } from 'react-instantsearch-hooks-web';
 import type {InstantSearchProps} from 'react-instantsearch-hooks-web';
 import connectAutocomplete from 'instantsearch.js/es/connectors/autocomplete/connectAutocomplete';
-import { createInsightsMiddleware } from 'instantsearch.js/es/middlewares';
+import {createInsightsMiddleware} from 'instantsearch.js/es/middlewares';
 
 
 import('@docsearch/react/style');
@@ -29,17 +29,18 @@ const searchClient = algoliasearch('0LUM67N2P2', '129d0f429e1bb0510f0261dda1e88e
 //   setUiState(uiState);
 // };
 
-function CustomSearchBox(props) {
-  const { query, refine, clear, isSearchStalled } = useSearchBox(props);
-
-  return <>{/* Your JSX */}</>;
-}
-
 // const configureApi = useConfigure({
 //   hitsPerPage: 4,
 //   analytics: false,
 //   distinct: true,
 // });
+
+const middleware = createInsightsMiddleware({
+  insightsClient: InsightsClient | null,
+  // Optional parameters
+  insightsInitParams: object,
+  onEvent: function,
+})
 
 function CustomConfigure(props) {
   useConfigure(props);
@@ -52,7 +53,7 @@ export function useAutocomplete(props) {
 }
 
 function InsightsMiddleware() {
-  const { use } = useInstantSearch();
+  const {use} = useInstantSearch();
 
   useLayoutEffect(() => {
     const middleware = createInsightsMiddleware({
@@ -78,74 +79,73 @@ function InsightsMiddleware() {
   return null;
 }
 
-function Hit({hit, children}) {
-  console.log(hit);
-  return <div className='search-hit'>
-    <Link to={hit.url}>{children}</Link>
-    <div className='search-hit-preview'>
-      {JSON.stringify(hit)}
+
+function InstantSearchHits() {
+  const {hits, sendEvent} = useHits();
+  return hits.slice(-2).map((hit, i) => {
+    console.log(hit);
+    return <div key={i} className='search-hit'>
+      <Link to={hit.url}>
+        <Highlight hit={hit} attribute='content' />
+      </Link>
     </div>
-  </div>;
+  })
+  r
 }
 
-function InstantSearchHit({hit}) {
-  const { hits, sendEvent } = useHits();
-
-  return (
-    <article>
-      <img src={hit.image} alt={hit.name} />
-      <p>{hit.categories[0]}</p>
-      <h1>
-        <Highlight attribute="name" hit={hit} />
-      </h1>
-      <p>${hit.price}</p>
-    </article>
-  );
-}
-
-function middleware({ instantSearchInstance }) {
-  return {
-    onStateChange({ uiState }) {
-      // Do something with `uiState` every time the state changes.
-    },
-    subscribe() {
-      // Do something when the InstantSearch instance starts.
-    },
-    unsubscribe() {
-      // Do something when the InstantSearch instance is disposed.
-    }
-  }
-}
-
-function Middleware() {
-  const { use } = useInstantSearch();
-
-  React.useLayoutEffect(() => {
-    return use(middleware);
-  });
+const onStateChange = ({uiState, setUiState}) => {
+  setUiState(uiState);
 }
 
 export function UnifiedSearchModal(props) {
-  return <div className={'unified-search-modal'}>
-    <div>
-      {/*<DocSearchModal {...props} hitComponent={Hit} />*/}
-      <InstantSearch searchClient={searchClient} indexName="near">
-        <Breadcrumb
-          attributes={[
-            'categories.lvl0',
-            'categories.lvl1',
-            'categories.lvl2',
-            'categories.lvl3'
-          ]}
-        />
-        {/* onStateChange={/*onStateChange!*/}
-        {/*<Configure hitsPerPage={40} />*/}
-        {/*<Middleware />*/}
-        {/*<CustomConfigure {...searchParameters} />*/}
-        <SearchBox />
-        {/*<CustomSearchBox />*/}
-        <Hits hitComponent={Hit} />
-      </InstantSearch>
+
+  return <div className={'usm-container'}>
+    <div className={'unified-search-modal'}>
+      <div className={'usm-content'}>
+        <InstantSearch searchClient={searchClient} indexName="near-staging"
+                       onStateChange={onStateChange}>
+          <Configure hitsPerPage={10} />
+          {/*<Breadcrumb*/}
+          {/*  attributes={[*/}
+          {/*    'lvl0',*/}
+          {/*    'lvl1',*/}
+          {/*    'lvl2',*/}
+          {/*    'lvl3',*/}
+          {/*    'lvl4',*/}
+          {/*    'lvl5',*/}
+          {/*  ]}*/}
+          {/*/>*/}
+          <Configure hitsPerPage={10} />
+          <div className={'usm-header'}>
+            <div className={'usm-searchbox'}>
+              <SearchBox autoFocus={true} classNames={{
+                root: 'usm-searchbox-root',
+                form: 'usm-searchbox-form',
+                input: 'usm-searchbox-input',
+                submit: 'usm-searchbox-submit',
+                reset: 'usm-searchbox-reset',
+                loadingIndicator: 'usm-searchbox-loadingIndicator',
+                submitIcon: 'usm-searchbox-submitIcon',
+                resetIcon: 'usm-searchbox-resetIcon',
+                loadingIcon: 'usm-searchbox-loadingIcon',
+              }} placeholder='Search docs' />
+            </div>
+          </div>
+          <div className={'usm-hits-tabs'}>
+          </div>
+          <div className={'usm-hits'}>
+            <div className={'usm-hits-list'}>
+              <InstantSearchHits />
+            </div>
+            <div className={'usm-hits-panel'}>
+              <h1>HITS</h1>
+            </div>
+          </div>
+          <div className={'usm-footer'}>
+            {/*<h1>FOOTER</h1>*/}
+          </div>
+        </InstantSearch>
+      </div>
     </div>
   </div>
 }
