53e4b65af7e3363b6d0020f6621f62d0e061663a
Damian Parrino <bucanero@users.noreply.github.com>
2023-08-02 16:49:24 -0300

    revert changes
    revert

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,7 +15,6 @@ module.exports = {
     "https://use.fontawesome.com/221fd444f5.js",
     "/js/copy-code-button.js",
     "/js/mixpanel.js",
-    "/js/chimp.js",
   ],
   stylesheets: [
     "https://fonts.googleapis.com/css2?family=Inter:wght@400;600;900&family=Source+Code+Pro:ital,wght@0,400;0,600;1,400;1,600&display=swap",
--- website/static/js/chimp.js
+++
@@ -1,2 +0,0 @@
-// 
-(!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)})(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9a67f4eac197159cd85f8f6b6/4acc7dcbd46d7f0943b3f3d3a.js");
