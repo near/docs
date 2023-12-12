92e11fa990a0bc82c2172a8091ba0a884ac95f33
gagdiez <gagdiez.c@gmail.com>
2023-12-12 16:33:16 +0100

    feat: new top menu (#1622)
    * feat: new menu
    
    * fix: fastauth link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -143,20 +143,25 @@ ${content}`, // <-- this last part adds in the rest of the content, which would 
           items: [
             { label: 'Smart Contracts', href: '/develop/contracts/welcome' },
             { label: 'Web3 Applications', href: '/develop/integrate/welcome' },
-            { label: 'Indexing the Chain', href: '/develop/monitor' },
+            { label: 'Data Solutions', href: '/develop/monitor' },
             {
               type: 'html',
-              value: '<hr/>',
+              value: '<hr/> <small class="subtitle"> Integrations </small>',
             },
-            {
-              label: 'Data Availability',
-              href: '/data-availability/welcome',
-            },
-            {
-              label: 'Exchange Integration',
-              href: '/integrator/exchange-integration',
-            },
+            { label: "Backend", href: "/develop/integrate/backend-login" },
+            { label: "Frontend", href: "/develop/integrate/frontend" },
+            { label: "Exchanges", href: "/integrator/exchange-integration" },
+            { label: "Rollups DA", href: "/data-availability/welcome" },
           ],
+        },
+        {
+          label: "Primitives",
+          position: "left",
+          items: [
+            { label: "Fungible Tokens", href: "/primitives/nft/introduction" },
+            { label: "Non-Fungible Tokens", href: "/primitives/ft/introduction" },
+            { label: "Decentralized Exchanges", href: "/primitives/dex/introduction" },
+          ]
         },
         {
           type: 'dropdown',
@@ -164,16 +169,27 @@ ${content}`, // <-- this last part adds in the rest of the content, which would 
           position: 'left',
           items: [
             { label: '🧰 All Tools', href: '/tools/welcome' },
+            {
+              type: 'html',
+              value: '<hr/> <small class="subtitle"> Essentials </small>',
+            },
             { label: 'NEAR API', href: '/tools/near-api-js/quick-reference' },
             { label: 'NEAR SDK', href: '/sdk/welcome' },
             { label: 'NEAR CLI', href: '/tools/near-cli' },
-            { label: 'Wallet Selector', href: '/tools/wallet-selector' },
-            { label: 'BOS Web IDE (Jutsu)', href: 'https://jutsu.ai/editor' },
             {
-              label: 'Remix IDE Plugin',
-              href: 'https://docs.welldonestudio.io/code/getting-started',
+              type: 'html',
+              value: '<hr/> <small class="subtitle"> Onboarding </small>',
             },
-          ],
+            { label: "Wallet Selector", href: "/tools/wallet-selector" },
+            { label: "FastAuth (Email Login)", href: "/tools/fastauth-sdk" },
+            { label: "Relayers", href: "/develop/relayers/build-relayer" },
+            {
+              type: 'html',
+              value: '<hr/> <small class="subtitle"> IDEs </small>',
+            },
+            { label: "VSCode Extension ", href: "/bos/dev/vscode" },
+            { label: "BOS Web IDE (Jutsu)", href: "https://jutsu.ai/editor" },
+            { label: "Remix IDE Plugin", href: "https://docs.welldonestudio.io/code/getting-started" },],
         },
         {
           href: '/api/rpc/introduction',
@@ -192,7 +208,7 @@ ${content}`, // <-- this last part adds in the rest of the content, which would 
               href: 'https://near-nodes.io',
               label: 'Validator Docs',
             },
-          ], 
+          ],
         },
         {
           type: 'search',
