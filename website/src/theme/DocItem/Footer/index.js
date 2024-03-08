64f8afb2b685a10e21a75c5f4190817a5c48f79e
Damián Parrino <bucanero@users.noreply.github.com>
2024-03-08 14:10:54 -0300

    add rating component (#1786)
    * add rating component
    
    * update style

---
+++ website/src/theme/DocItem/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '@theme-original/DocItem/Footer';
+import {FeedbackComponent } from "../../../components/FeedbackComponent";
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <Footer {...props} />
+
+      <div class="theme-admonition theme-admonition-tip admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module alert alert--info">
+      <FeedbackComponent />
+      </div>
+    </>
+  );
+}
