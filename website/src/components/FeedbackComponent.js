6e85eb1c97f422007c774948a4e02a5ff88a1938
Josh Ford <thisisjoshford@gmail.com>
2024-03-21 20:25:44 -0700

    cleanup descriptions

--- website/src/components/FeedbackComponent.js
+++ website/src/components/FeedbackComponent.js
@@ -1,43 +1,51 @@
 import { useState } from "react";
-import { FeelbackTaggedMessage, FeelbackValueDefinition, Question, PRESET_YESNO_LIKE_DISLIKE } from "@feelback/react";
+import {
+  FeelbackTaggedMessage,
+  FeelbackValueDefinition,
+  Question,
+  PRESET_YESNO_LIKE_DISLIKE,
+} from "@feelback/react";
 import "@feelback/react/styles/feelback.css";
 
 const YES_TAGS = [
-    { value: "accurate", title: "Accurate", description: "Accurately describes the product or feature.", },
-    { value: "problem-solved", title: "Solved my problem", description: "Helped me resolve an issue.", },
-    { value: "clear", title: "Easy to understand", description: "Easy to follow and comprehend.", },
-    { value: "product-chosen", title: "Helped me decide to use the product", description: "Convinced me to adopt the product or feature.", },
-    { value: "other-yes", title: "Another reason" },
+  { value: "accurate", title: "Accurate" },
+  { value: "problem-solved", title: "Solved my problem" },
+  { value: "clear", title: "Easy to understand" },
+  { value: "product-chosen", title: "Helped me decide to use this tech" },
+  { value: "other-yes", title: "Other" },
 ];
 
 const NO_TAGS = [
-    { value: "inaccurate", title: "Inaccurate", description: "Doesn't accurately describe the product or feature.", },
-    { value: "missing-info", title: "Couldn't find what I was looking for", description: "Missing important information.", },
-    { value: "unclear", title: "Hard to understand", description: "Too complicated or unclear.", },
-    { value: "bad-examples", title: "Code samples errors", description: "One or more code samples are incorrect.", },
-    { value: "other-no", title: "Another reason" },
+  { value: "inaccurate", title: "Inaccurate" },
+  { value: "missing-info", title: "Couldn't find what I was looking for" },
+  { value: "unclear", title: "Hard to understand" },
+  { value: "bad-examples", title: "Code samples errors" },
+  { value: "other-no", title: "Other" },
 ];
 
 const FEEDBACK_CONTENT_SET_ID = "6d4fac2e-6797-47aa-8d5c-b9318a0655e8";
 
 export function FeedbackComponent() {
-    const [choice, setChoice] = useState();
+  const [choice, setChoice] = useState();
 
-    return (
-        <div className="feelback-container">
-            {!choice
-                ? <Question text="Was this page helpful?"
-                    items={PRESET_YESNO_LIKE_DISLIKE}
-                    showLabels
-                    onClick={setChoice}
-                />
-                : <FeelbackTaggedMessage contentSetId={FEEDBACK_CONTENT_SET_ID}
-                    layout="radio-group"
-                    tags={choice === "y" ? YES_TAGS : NO_TAGS}
-                    title={choice === "y" ? "What did you like?" : "What went wrong?"}
-                    placeholder="(optional) Please, further detail the feedback"
-                />
-            }
-        </div>
-    );
+  return (
+    <div className="feelback-container">
+      {!choice ? (
+        <Question
+          text="Was this page helpful?"
+          items={PRESET_YESNO_LIKE_DISLIKE}
+          showLabels
+          onClick={setChoice}
+        />
+      ) : (
+        <FeelbackTaggedMessage
+          contentSetId={FEEDBACK_CONTENT_SET_ID}
+          layout="radio-group"
+          tags={choice === "y" ? YES_TAGS : NO_TAGS}
+          title={choice === "y" ? "What did you like?" : "What went wrong?"}
+          placeholder="(optional) Please, further detail the feedback"
+        />
+      )}
+    </div>
+  );
 }
