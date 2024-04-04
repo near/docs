92cb09087931ff315ca525c7f8f21418a0953eca
Damian Parrino <bucanero@users.noreply.github.com>
2024-04-04 17:19:03 -0300

    Update helpcomponent.js

--- website/src/components/helpcomponent.js
+++ website/src/components/helpcomponent.js
@@ -4,7 +4,24 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 export function HelpComponent({ children }) {
   return <>
 
-<h3> Looking for help? 👉 <a href="/help">Check our Resources</a></h3>
+<div class="theme-admonition theme-admonition-tip admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module alert alert--success">
+
+<details>
+<summary><b style={{cursor: 'pointer'}}>Looking for help? 👉 <a href="/help">Check our Resources</a> or <a>chat with us</a>!</b></summary>
+
+
+<div class="col">
+<br/>
+<a href="https://neardevhub.org/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://img.shields.io/badge/DEV_HUB-03BE09" alt="DEVHUB" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" /></a>
+&nbsp;
+<a href="https://bit.ly/near-dev-calendar" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://img.shields.io/badge/CALENDAR-F9F502" alt="CALENDAR" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" /></a>
+&nbsp;
+<a href="https://t.me/addlist/VyVjNaP190JlOGMx" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://img.shields.io/badge/DEV_SUPPORT-BE0303" alt="DEV SUPPORT" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" /></a>
+&nbsp;
+<a href="https://newsletter.neardevhub.org/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://img.shields.io/badge/NEWSLETTER-0087E5" alt="NEWSLETTER" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" /></a>
+&nbsp;
+<a href="https://github.com/orgs/near/discussions/new?category=dev-feedback" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://img.shields.io/badge/FEEDBACK-purple" alt="FEEDBACK" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" /></a>
+</div>
 
 <div class="row">
   <div class="col col--3">
@@ -47,6 +64,9 @@ export function HelpComponent({ children }) {
       </div>
     </div>
   </div>
+</div>
+</details>
+
 </div>
 
   </>
