6f8aa8b0f939d8cff45ba3c3e22bb5b15c44f344
Matias Benary <matiasbenary@gmail.com>
2024-08-01 13:03:42 -0300

    fix: build check

--- website/src/theme/Navbar/Logo/index.js
+++ website/src/theme/Navbar/Logo/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import Logo from '@theme/Logo';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function NavbarLogo() {
-  return (
-    <BrowserOnly>
-      {() => {
-        // if embedded in an iframe, do not show the logo
-        if (typeof window !== 'undefined' && window.self !== window.top) {
-          return null;
-        }
+  const isBrowser = useIsBrowser();
+  // if embedded in an iframe, do not show the logo
+  if (isBrowser && typeof window !== 'undefined' && window.self !== window.top) return null;
 
-        return (
-          <Logo
-            className="navbar__brand"
-            imageClassName="navbar__logo"
-            titleClassName="navbar__title text--truncate"
-          />
-        );
-      }}
-    </BrowserOnly>
+  return (
+    <Logo
+      className="navbar__brand"
+      imageClassName="navbar__logo"
+      titleClassName="navbar__title text--truncate"
+    />
   );
 }
