7a45026b463aab35db5ee2486aada9378920303b
Damian Parrino <bucanero@users.noreply.github.com>
2024-11-26 12:26:51 -0300

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -281,15 +281,6 @@ const config = {
       insights: true,
     },
   },
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ko', 'vi', 'zh-CN'],
-    localeConfigs: {
-      'zh-CN': {
-        label: '简体中文',
-      },
-    },
-  },
 };
 
 export default config;
