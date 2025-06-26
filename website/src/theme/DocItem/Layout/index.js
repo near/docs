b465aac35752de225f3fdc53307664156d97c828
Josh Ford <thisisjoshford@gmail.com>
2025-06-26 08:56:41 -0700

    fix: kill HelpComponent

--- website/src/theme/DocItem/Layout/index.js
+++ website/src/theme/DocItem/Layout/index.js
@@ -54,7 +54,6 @@ export default function DocItemLayout({ children }) {
           <DocItemPaginator />
           <FeedbackComponent />
           <AIChat />
-          <HelpComponent />
           <DocItemFooter />
         </div>
       </div>
