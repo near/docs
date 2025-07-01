8837c869189e5c260fdc9b635a0bd9fab17a70e1
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-07-01 13:45:18 +0200

    fix: build

--- website/src/theme/DocItem/Layout/index.js
+++ website/src/theme/DocItem/Layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import { useWindowSize } from '@docusaurus/theme-common';
 import { useDoc } from '@docusaurus/plugin-content-docs/client';
@@ -13,7 +13,6 @@ import DocBreadcrumbs from '@theme/DocBreadcrumbs';
 import ContentVisibility from '@theme/ContentVisibility';
 import styles from './styles.module.css';
 
-import { HelpComponent } from '../../../components/helpcomponent';
 import { FeedbackComponent } from '../../../components/FeedbackComponent';
 import AIChat from '../../../components/AIChat';
 
@@ -39,6 +38,12 @@ function useDocTOC() {
 export default function DocItemLayout({ children }) {
   const docTOC = useDocTOC();
   const { metadata } = useDoc();
+  const [isMain, setIsMain] = React.useState(true);
+
+  useEffect(() => {
+    setIsMain(window.location.pathname === '/');
+  }, []);
+
   return (
     <div className="row">
       <div className={clsx('col', !docTOC.hidden && styles.docItemCol)}>
@@ -51,7 +56,7 @@ export default function DocItemLayout({ children }) {
             {docTOC.mobile}
             <DocItemContent>{children}</DocItemContent>
           </article>
-          {(window.location.pathname !== '/') && <>
+          {!isMain && <>
             <FeedbackComponent />
             <DocItemPaginator />
             <DocItemFooter />
