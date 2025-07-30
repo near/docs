001b96e1720021defd450ff7a82b608eb3fc5ea1
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-07-30 18:29:20 +0200

    feat: login button for near testnet

---
+++ website/src/plugins/node-polyfills/index.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+// eslint-disable-next-line
+module.exports = function (context, options) {
+  return {
+    name: 'node-polyfills',
+    // eslint-disable-next-line
+    configureWebpack(config, isServer, utils) {
+      return {
+        plugins: [
+          new webpack.ProvidePlugin({
+            process: 'process/browser.js',
+            Buffer: ['buffer', 'Buffer'],
+          }),
+        ],
+        resolve: {
+          alias: {},
+          fallback: {
+            crypto: require.resolve('crypto-browserify'),
+            // http: require.resolve('stream-http'),
+            // https: require.resolve('https-browserify'),
+            // buffer: require.resolve('buffer'),
+            stream: require.resolve('stream-browserify'),
+            vm: require.resolve('vm-browserify'),
+            // url: require.resolve('url'),
+          },
+        },
+      };
+    },
+  };
+};
