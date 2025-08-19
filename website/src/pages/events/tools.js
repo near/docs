e0542b262f80a1ca57e622ad9c44c76eeb110842
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-08-19 19:35:25 +0200

    fix: simplify newsletter logic

--- website/src/pages/events/tools.js
+++ website/src/pages/events/tools.js
@@ -1,15 +1,8 @@
-export const fetchLumaEvents = async (
-  calendarApiId,
-  limit,
-  offset,
-  lumaApiUrl = 'http://localhost:5000',
-) => {
-  const queryFrom = `period=future`;
-  const queryLimit = `pagination_limit=${limit ?? 10}`;
-  const queryOffset = offset ? `pagination_offset=${offset}` : '';
-  const queryParams = [queryFrom, queryLimit, queryOffset].filter(Boolean).join('&');
+export const fetchLumaEvents = async (lumaCalendarId) => {
 
-  const res = await fetch(`${lumaApiUrl}/api/calendar/calendar_api_id=${calendarApiId}?${queryParams}`, {
+  console.log('Fetching Luma Events from: Calendar ID:', lumaCalendarId);
+
+  const res = await fetch(`https://tmp-docs-ai-service.onrender.com/api/calendar/${lumaCalendarId}`, {
     method: 'GET',
     headers: {
       accept: 'application/json',
@@ -17,33 +10,80 @@ export const fetchLumaEvents = async (
   });
 
   if (!res.ok) {
-    throw new Error('Failed to fetch Luma Calendar Data');
+    console.error('Failed to fetch Luma Calendar Data');
+    return { entries: [], hasMore: false };
   }
 
-  const data = await res.json();
+  const { data } = await res.json();
 
-  return { entries: data.entries, hasMore: data.has_more };
+  if (!data || !data.entries) {
+    return { entries: [], hasMore: false };
+  }
+
+  let allEvents = data.entries;
+
+  const sortedEvents = allEvents
+    .sort((a, b) => new Date(a.event.start_at).getTime() - new Date(b.event.start_at).getTime())
+
+  const formatLocation = (location) => {
+    if (location.city || location.city_state) {
+      return `${location.city ?? location.city_state}, ${location.country}`;
+      // Luma API returns "mode" as "obfuscated" when the address is hidden
+    } else if (location && location?.mode === 'obfuscated') {
+      return 'Register to See Address';
+    }
+    return location.address;
+  };
+
+  const formattedEvents = sortedEvents.map((item) => {
+    return {
+      id: item.event.api_id,
+      start: item.event.start_at,
+      location: formatLocation(item.event.geo_address_json ?? item.event.geo_address_info ?? ''),
+      thumbnail: item.event.cover_url,
+      title: item.event.name,
+      url: `https://lu.ma/${item.event.url}`,
+    };
+  });
+
+  console.log('Luma Calendar Formatted Data:', formattedEvents);
+
+  return { entries: formattedEvents, hasMore: data.has_more };
 };
 
-export const fetchGoogleCalendarEvents = async (
-  calendarId,
-  startFrom,
-  limit,
-  pageToken,
-  googleCalendarApiKey
-) => {
+export const fetchGoogleCalendarEvents = async (googleCalendarId, googleCalendarApiKey) => {
+  const startFrom = new Date().toISOString();
+
   const res = await fetch(
-    `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${googleCalendarApiKey}&maxResults=${limit}&timeMin=${startFrom}&singleEvents=true&orderBy=startTime&pageToken=${pageToken}`,
+    `https://www.googleapis.com/calendar/v3/calendars/${googleCalendarId}/events?key=${googleCalendarApiKey}&maxResults=9&timeMin=${startFrom}&singleEvents=true&orderBy=startTime`,
     {},
   );
 
   if (!res.ok) {
-    throw new Error('Failed to fetch Google Calendar Data');
+    console.error('Failed to fetch Google Calendar Data');
+    return { items: [] };
   }
 
-  const data = await res.json();
+  let { items } = await res.json();
 
-  return { items: data.items, nextPageToken: data.nextPageToken };
+  // flatten the items
+  items = [...items].flat();
+
+  const formattedEvents = items.map((event) => {
+    return {
+      id: event.id,
+      title: event.summary,
+      start: event.start.dateTime,
+      thumbnail: `https://lh3.googleusercontent.com/d/${event.attachments?.[0]?.fileId}=w1000`,
+      location: '',
+      url: event.htmlLink,
+    };
+  });
+
+  console.log('Google Calendar Formatted Data:', formattedEvents);
+
+
+  return { items: formattedEvents };
 };
 
 export const formatEventDateTime = (dateString) => {
