d69b7d18bcaf704c23cbff9ba6c8e0c8143409ee
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-08-19 15:56:02 +0200

    fix: minor fixes

--- website/src/theme/NavbarItem/index.js
+++ website/src/theme/NavbarItem/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
 import NavbarItem from '@theme-original/NavbarItem';
 import LoginButton from './login-button'
-import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
 
 export default function NavbarItemWrapper(props) {
-    if (props.href === 'login') {
-    return <LoginButton {...props} />
-  } else {
-    const newProps = { ...props };
-    if ('subitems' in props) newProps.items = props.subitems; // Backward compatibility for subitems
-    return <NavbarItem {...newProps} />
-  }
+  if (props.href === 'login') return <LoginButton {...props} />
+
+  const newProps = { ...props };
+  if ('subitems' in props) newProps.items = props.subitems; // Backward compatibility for subitems
+  return <NavbarItem {...newProps} />
 }
