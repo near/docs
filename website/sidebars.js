1ee5de506f58bca818d66e812aa7412af17fcc9b
Guille <gagdiez.c@gmail.com>
2025-10-30 15:59:02 +0100

    Update website/sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -396,7 +396,6 @@ const sidebar = {
                 'web3-apps/tutorials/web-login/near-connector',
                 'web3-apps/tutorials/web-login/wallet-selector',
                 'web3-apps/tutorials/web-login/ethereum-wallets',
-                'web3-apps/tutorials/web-login/near-conector',
                 'web3-apps/tutorials/web-login/web3-auth',
               ]
             },
